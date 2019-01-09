68a7f4dadc5723ef543e95fd9b74b96043852dfc
aidenBarrett96 <46399495+aidenBarrett96@users.noreply.github.com>
2019-01-09 14:01:35 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,203 +1,144 @@
-<!--<?php
- class song {
-     
-     var $title, $artist, $album, $lyric, $start, $finish;
-     
-     function setDetails($newTitle, $newArtist, $album, $newLyric, $newStart, $newFinish) {
-         $this->title = $newTitle;
-         $this->artist = $newArtist;
-         $this->album = $newAlbum;
-         $this->lyric = $newLyric;
-         $this->start = $newStart;
-         $this->finish = $newFinish;
-     } 
-     
-     function getTitle() {
-         return $this->title;
-     }
-     
-          function getArtist() {
-         return $this->artist;
-     }
-     
-          function getAlbum() {
-         return $this->album;
-     }
-     
-          function getLyric() {
-         return $this->lyric;
-     }
-     
-          function getStart() {
-         return $this->start;
-     }
-     
-          function getFinish() {
-         return $this->finish;
-     }
-     
-     
- }
- 
-?>-->
 <html>
 <head>
-    <title>My Page</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">	
+    	<title>My Page</title>
+    	<meta name="viewport" content="width=device-width, initial-scale=1">	
 	<script src="functions.js" type="text/javascript"></script>
-<!--  
-  
-
-<script src="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.js"></script>
-
-
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
--->		<link rel="stylesheet" href="themes/jquery.mobile.icons.min.css" />
+	<link rel="stylesheet" href="themes/jquery.mobile.icons.min.css" />
 	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile.structure-1.4.5.min.css" />
-
 	<link rel="stylesheet" href="themes/snippetKingTheme.css" />
 	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 	<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
-<script src="jquery.ui.touch-punch.min.js"></script>
+	<script src="jquery.ui.touch-punch.min.js"></script>
 
 
-<link href='https://fonts.googleapis.com/css?family=Bowlby One SC' rel='stylesheet'>
-<link href='https://fonts.googleapis.com/css?family=Cabin Condensed' rel='stylesheet'>
-<script>
+	<link href='https://fonts.googleapis.com/css?family=Bowlby One SC' rel='stylesheet'>
+	<link href='https://fonts.googleapis.com/css?family=Cabin Condensed' rel='stylesheet'>
 
-
-
-		
+	<script>
 		$( function() {
-		
 			$(".draggable").draggable({
-			revert: function(event, ui) {
-            // on older version of jQuery use "draggable"
-            // $(this).data("draggable")
-            // on 2.x versions of jQuery use "ui-draggable"
-            // $(this).data("ui-draggable")
-           /* $(this).data("uiDraggable").originalPosition = {
-                top : 0,
-                left : 0
-            };*/
-            // return boolean
-            return !event;
-            // that evaluate like this:
-            // return event !== false ? false : true;
-        }
-		});			
+				revert: function(event, ui) {
+            				// on older version of jQuery use "draggable"
+				    	// $(this).data("draggable")
+				    	// on 2.x versions of jQuery use "ui-draggable"
+				    	// $(this).data("ui-draggable")
+				   	/* $(this).data("uiDraggable").originalPosition = {
+						top : 0,
+						left : 0
+				    	};*/
+					
+            				// return boolean
+            				return !event;
+            				// that evaluate like this:
+            				// return event !== false ? false : true;
+        			}
+			});			
 			
-			
-		
 			$(".letters").draggable({
-			revert: function(event, ui) {
-            // on older version of jQuery use "draggable"
-            // $(this).data("draggable")
-            // on 2.x versions of jQuery use "ui-draggable"
-            // $(this).data("ui-draggable")
-           /* $(this).data("uiDraggable").originalPosition = {
-                top : 0,
-                left : 0
-            };*/
-            // return boolean
-            return !event;
-            // that evaluate like this:
-            // return event !== false ? false : true;
-        }
-		});			
+				revert: function(event, ui) {
+					// on older version of jQuery use "draggable"
+				    	// $(this).data("draggable")
+				    	// on 2.x versions of jQuery use "ui-draggable"
+				    	// $(this).data("ui-draggable")
+				   	/* $(this).data("uiDraggable").originalPosition = {
+						top : 0,
+						left : 0
+				    	};*/
+					
+				    	// return boolean
+				    	return !event;
+				    	// that evaluate like this:
+				    	// return event !== false ? false : true;
+        			}
+			});			
 		
 		
-		$("#testDrop").droppable();
+			$("#testDrop").droppable();
 		
 		});
 
-	
-
-
-</script>
-    
-
+	</script>
 </head>
+	
 <body>
+	<div data-role="page" id="tapToContinue_page">
+		<div data-role="header">
+			<h1><a href="#" class="appTitle">Snippet Kings</a></h1>
+		</div><!-- /header -->
+ 
+		<div role="main" class="ui-content" id="test1">
+			<div id="test2">
+				<a href="#login_page" data-transition="fade">Tap to continue</a>
+			</div>
+		</div><!-- /content -->
 
-<div data-role="page" id="tapToContinue_page">
- 
-	<div data-role="header">
-<h1><a href="#" class="appTitle">Snippet Kings</a></h1>
-    </div><!-- /header -->
- 
-	<div role="main" class="ui-content" id="test1">
-            <div id="test2">
-            	<a href="#login_page" data-transition="fade">Tap to continue</a>
-            </div>
-    </div><!-- /content -->
-
- </div><!-- /page -->
+ 	</div><!-- /page -->
  
  
- <!--Login or continue as guest page-->
- <div data-role="page" id="login_page">
- 
- 	<div data-role="header">
-		<h1><a href="#" class="appTitle">Snippet Kings</a></h1>
-    </div><!-- /header -->
-  
-   <div data-role="content">
-   
-      
-      <div data-role="collapsible-set">
-      	<!--Login-->
-      	<div data-role="collapsible">
-      		<h3>Login</h3>
-              <form>
-              	<div class="ui-field-contain">
-    					<label for="username_log">Enter Username:</label>
-   					 	<input type="text" name="username_log" id="username_log" placeholder="Username" value="">
+	<!--Login or continue as guest page-->
+	<div data-role="page" id="login_page">
+		<div data-role="header">
+			<h1><a href="#" class="appTitle">Snippet Kings</a></h1>
+		</div><!-- /header -->
+		
+		<div data-role="content">
+			<div data-role="collapsible-set">
+				<!--Login-->
+				<div data-role="collapsible">
+					<h3>Login</h3>
+					
+					<form>
+						<div class="ui-field-contain">
+							<label for="username_log">Enter Username:</label>
+							<input type="text" name="username_log" id="username_log" placeholder="Username" value="">
+						</div>
+						
+						<div class="ui-field-contain">
+							<label for="password_log">Enter Password:</label>
+							<input type="text" name="password_log" id="username_log" placeholder="Password" value="">
+						</div>
+						
+						<input type="submit" name="submit_log" id="submit_log" value="Log In">
+						
+					</form>
 				</div>
-                <div class="ui-field-contain">
-    					<label for="password_log">Enter Password:</label>
-   					 	<input type="text" name="password_log" id="username_log" placeholder="Password" value="">
+				
+				<div data-role="collapsible">
+					<h3>Create an account</h3>
+					<form>
+						<div class="ui-field-contain">
+							<label for="username_reg">Enter Username:</label>
+							<input type="text" name="username_reg" id="username_reg" placeholder="Username" value="">
+						</div>
+						<div class="ui-field-contain">
+							<label for="password_reg">Enter Password:</label>
+							<input type="text" name="password_reg" id="username_reg" placeholder="Password" value="">
+						</div>
+						<div class="ui-field-contain">
+							<label for="passwordVer_reg">Verify Password:</label>
+							<input type="text" name="passwordVer_reg" id="passwordVer_reg" placeholder="Password" value="">
+						</div>
+						
+						<div class="ui-field-contain">
+							<label for="email_reg">Email Address:</label>
+							<input type="text" name="email_reg" id="email_reg" placeholder="Email" value="">
+						</div>
+						
+						<input type="submit" value="Register" name="submit_reg" id="submit_reg">
+						
+					</form>
 				</div>
-                
-                <input type="submit" name="submit_log" id="submit_log" value="Log In">
-
-              </form>
-      	</div>
-      
-      	<div data-role="collapsible">
-      		<h3>Create an account</h3>
-            	<form>
-              	<div class="ui-field-contain">
-    					<label for="username_reg">Enter Username:</label>
-   					 	<input type="text" name="username_reg" id="username_reg" placeholder="Username" value="">
-				</div>
-                <div class="ui-field-contain">
-    					<label for="password_reg">Enter Password:</label>
-   					 	<input type="text" name="password_reg" id="username_reg" placeholder="Password" value="">
-				</div>
-                <div class="ui-field-contain">
-    					<label for="passwordVer_reg">Verify Password:</label>
-   					 	<input type="text" name="passwordVer_reg" id="passwordVer_reg" placeholder="Password" value="">
-				</div>
-                <div class="ui-field-contain">
-    					<label for="email_reg">Email Address:</label>
-   					 	<input type="text" name="email_reg" id="email_reg" placeholder="Email" value="">
-				</div>
-                
-                <input type="submit" value="Register" name="submit_reg" id="submit_reg">
-                
-              </form>
-     	 </div>
-         
-      
-      </div>
-      
-      <a data-role="button" href="#mainMenu_page">Continue as guest</a>
-   
-   </div>
- 
- </div>
+			</div>
+			
+			<a data-role="button" href="#mainMenu_page">Continue as guest</a>
+			
+		</div>
+	</div>
+	
+	
+	
+	<!--Checkpoint-->
  
     
 <!--main menu page-->
